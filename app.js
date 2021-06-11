f023ec4230d6bc5b1bc622a66c927df105522d43
Jejoon Ryu <rjejoon@gmail.com>
2021-06-11 14:35:09 -0600

    Add btn click animation

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
+const books = document.querySelectorAll('.book');
+books.forEach(book => {
+    book.addEventListener('mouseenter', bookEnterHandler)
+    book.addEventListener('mouseleave', bookLeaveHandler)
+    book.querySelector('.done-icon').addEventListener('click', doneClickHandler); 
+});
+
+function bookEnterHandler(e) {
+    this.querySelector('.button-container').classList.add('visible');
+}
+
+function bookLeaveHandler(e) {
+    this.querySelector('.button-container').classList.remove('visible');
+}
+
+function doneClickHandler(e) {
+    this.classList.toggle('done-read');
+}
+
+
+
+
 let myLibrary = [];
 
-function Book() {
+function Book(title='', author='', pages='', isRead=false) {
     // constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = isRead;
 }
 
 function addBookToLibrary() {
 
 }
+
+
 
 

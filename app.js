1b52919227f73e5e3ab8679378f258380dab0def
Jejoon Ryu <rjejoon@gmail.com>
2021-06-12 22:41:21 -0600

    Get book info from form element

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const books = document.querySelectorAll('.book');
 const addBookEle = document.querySelector('#add-book');
+const addBookForm = document.querySelector('#add-book-form');
 
 books.forEach(book => {
-    book.addEventListener('mouseenter', bookEnterHandler)
-    book.addEventListener('mouseleave', bookLeaveHandler)
-    book.querySelector('.done-icon').addEventListener('click', doneClickHandler); 
+    if (book.id != 'add-book') {
+        book.addEventListener('mouseenter', bookEnterHandler)
+        book.addEventListener('mouseleave', bookLeaveHandler)
+        book.querySelector('.done-icon').addEventListener('click', doneClickHandler); 
+    }
 });
 
 addBookEle.addEventListener('mouseenter', addBookEnterHandler);
 addBookEle.addEventListener('mouseleave', addBookLeaveHandler);
+
+addBookForm.addEventListener('submit', getBookInfo);
 
 function bookEnterHandler(e) {
     this.querySelector('.button-container').classList.add('visible');
@@ -30,6 +35,15 @@ function addBookLeaveHandler(e) {
     this.querySelector('.material-icons').classList.remove('rotate-icon');
 }
 
+function getBookInfo(e) {
+    e.preventDefault();
+    const title = this.elements[0].value;
+    const author = this.elements[1].value;
+    const pages = this.elements[2].value;
+    const isRead = this.elements[3].checked;
+
+    console.log(title, author, pages, isRead);
+}
 
 
 let myLibrary = [];

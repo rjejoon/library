bbd14fa2afcee4207188b1eed4d568c6a61f904d
Jejoon Ryu <rjejoon@gmail.com>
2021-06-15 12:26:25 -0600

    Implement add book functionality

--- app.js
+++ app.js
@@ -4,15 +4,8 @@ const addBookFormSubmitBtn = document.querySelector('.add-book-submit-btn');
 const addBookForm = document.querySelector('.add-book-form');
 const addBookFormBg = document.querySelector('.add-book-background');
 
-const books = document.querySelectorAll('.book');
+let myLibrary = [];
 
-books.forEach(book => {
-    if (!book.classList.contains('add-book')) {
-        book.addEventListener('mouseenter', bookEnterHandler);
-        book.addEventListener('mouseleave', bookLeaveHandler);
-        book.querySelector('.done-icon').addEventListener('click', doneClickHandler); 
-    }
-});
 
 window.addEventListener('DOMContentLoaded', e => {
 
@@ -27,6 +20,82 @@ window.addEventListener('DOMContentLoaded', e => {
     addBookFormBg.addEventListener('click', addBookFormBgClickHandler);
 });
 
+
+function Book(title='', author='', pages='', isRead=false) {
+    // constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    const title = this.elements[0].value;
+    const author = this.elements[1].value;
+    const pages = this.elements[2].value;
+    const isRead = this.elements[3].checked;
+
+    const book = new Book(title, author, pages, isRead);
+    myLibrary.push(book);
+
+    const bookEle = createBookElement(book, myLibrary.length-1);
+    libraryGrid.insertBefore(bookEle, addBookEle);
+
+    addBookFormBg.classList.remove('add-book-background-visible');  // hide form
+}
+
+function updateLibrary() {
+
+    const booksHTML = myLibrary.map((book, index) => getBookHTMLTemplate(book, index));
+
+    libraryGrid.innerHTML = booksHTML.join('');
+}
+
+
+function createBookElement({ title, author, pages, isRead }, index) {
+    const book = document.createElement('div');
+    book.classList.add('book')
+    book.dataset['index'] = index;
+
+    const bookInfoContainer = document.createElement('div');
+    bookInfoContainer.classList.add('book-info-container');
+
+    const titleEle = document.createElement('span');
+    titleEle.classList.add('title');
+    titleEle.textContent = title;
+
+    const authorEle = document.createElement('span');
+    authorEle.classList.add('author');
+    authorEle.textContent = author;
+
+    const pagesEle = document.createElement('span');
+    pagesEle.classList.add('pages');
+    pagesEle.textContent = `${pages} pages`;
+
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('button-container');
+    console.log(isRead);
+    btnContainer.innerHTML = `
+        <span class="material-icons done-icon ${isRead ? "done-read" : ""}">done</span>
+        <span class="material-icons">delete_outline</span>`;
+
+    bookInfoContainer.appendChild(titleEle);
+    bookInfoContainer.appendChild(authorEle);
+    bookInfoContainer.appendChild(pagesEle);
+    bookInfoContainer.appendChild(btnContainer);
+    
+    book.appendChild(bookInfoContainer);
+    addBookElementEvents(book);
+
+    return book;
+}
+
+function addBookElementEvents(book) {
+    book.addEventListener('mouseenter', bookEnterHandler);
+    book.addEventListener('mouseleave', bookLeaveHandler);
+    book.querySelector('.done-icon').addEventListener('click', doneClickHandler); 
+}
 
 function bookEnterHandler(e) {
     this.querySelector('.button-container').classList.add('visible');
@@ -59,51 +128,6 @@ function addBookFormBgClickHandler(e) {
 }
 
 
-let myLibrary = [];
-
-function Book(title='', author='', pages='', isRead=false) {
-    // constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = isRead;
-}
-
-function addBookToLibrary(e) {
-    e.preventDefault();
-    const title = this.elements[0].value;
-    const author = this.elements[1].value;
-    const pages = this.elements[2].value;
-    const isRead = this.elements[3].checked;
-
-    const book = new Book(title, author, pages, isRead);
-    myLibrary.push(book);
-    //updateLibrary();
-}
-
-function updateLibrary() {
-
-    const booksHTML = myLibrary.map((book, index) => getBookHTMLTemplate(book, index));
-
-    libraryGrid.innerHTML = booksHTML.join('');
-}
-
-
-function getBookHTMLTemplate({ title, author, pages, isRead }, index) {
-    return `
-      <div class="book" data-index="${index}">
-        <div class="book-info-container">
-          <span class="title">${title}</span>
-          <span class="author">${author}</span>
-          <span class="pages">${pages} pages</span>
-          <div class="button-container">
-            <span class="material-icons done-icon">done</span>
-            <span class="material-icons">delete_outline</span>
-          </div>
-        </div>
-      </div>
-    `;
-}
 
 
 

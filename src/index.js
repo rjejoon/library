62ec92994452f7eea3ca72b282a2d9242cfa27ff
Jejoon Ryu <rjejoon@gmail.com>
2021-11-10 06:07:35 -0700

    Use DOMManager to create app

--- src/index.js
+++ src/index.js
@@ -4,24 +4,9 @@ import styles from "./css/styles.css";
 import navbar from "./components/navbar/navbar.js";
 import library from "./components/library/library.js";
 
-import controller from "./controller.js";
+import controller from "./models/controller.js";
+import DOMManager from "./models/dommanager";
 
 
-(() => {
-  const body = document.querySelector("body");
-
-  const mainElement = document.createElement("main");
-  const libraryGrid = library.getLibraryGrid();
-  mainElement.appendChild(libraryGrid);
-
-  body.appendChild(navbar.getNavbarElement());
-  body.appendChild(mainElement);
-
-  controller.getAuthStateObserver();    
-
-  const signInBtn = navbar.getSignInBtn();
-  signInBtn.addEventListener("click", e => {
-    controller.signIn();
-  });
-
-})();
+DOMManager.createApp();
+controller.getAuthStateObserver();    

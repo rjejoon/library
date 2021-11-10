bec442e0c6fee63801cbc4ae9715bca27bcc18c2
Jejoon Ryu <rjejoon@gmail.com>
2021-11-10 08:33:40 -0700

    Remove unused bookConverter for firestore

--- src/models/book.js
+++ src/models/book.js
@@ -5,18 +5,3 @@ export function Book(title, author, pages, isRead) {
   this.pages = pages;
   this.isRead = isRead;
 }
-
-export const bookConverter = {
-  toFirestore: (book) => {
-    return {
-      title: book.title,
-      author: book.author,
-      pages: book.pages,
-      isRead: book.isRead,
-    };
-  },
-  fromFirestore: (snapshot, options) => {
-    const data = snapshot.data(options);
-    return new Book(data.title, data.author, data.pages, data.isRead);
-  }
-}
